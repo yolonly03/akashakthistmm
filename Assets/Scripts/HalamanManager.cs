1c5d090a0422b673355fca4bfa89b38dfbbe17db
yolonly03 <ngawurmeneh12@gmail.com>
2023-08-21 09:15:25 +0700

    pong2d
    tugas kelas lur

---
+++ Assets/Scripts/HalamanManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HalamanManager : MonoBehaviour
+{
+    public bool isEscapeToExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape)){
+            if(isEscapeToExit){
+                Application.Quit();
+            }else{
+                KembaliKeMenu();
+            }
+        }
+    }
+    public void MulaiPermainan(){
+        SceneManager.LoadScene("Main");
+    }
+    public void KembaliKeMenu(){
+        SceneManager.LoadScene("Menu");
+    }
+}
